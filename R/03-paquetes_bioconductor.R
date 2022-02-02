620dcfb8bdc22fc03dc7b7d7c59214269e46a629
zarams <zaram042001@gmail.com>
2022-02-01 20:09:14 -0600

    Se agregó información al trabajo

---
+++ R/03-paquetes_bioconductor.R
@@ -0,0 +1,86 @@
+## ACTIVIDAD GRUPAL: SELECCION DE PAQUETES
+
+# Paquete: CellBarcode
+#
+# Resumen: Análisis de códigos de barras de DNA.
+#
+# Utilidades: Puede ser usado en estudios de rastreo de linajes y seguimiento de
+# clones.
+#
+# Link: https://bioconductor.org/packages/CellBarcode/
+
+
+# Paquete: Methylclock - DNA methylation-based clocks
+#
+# Resumen: Estimar edad del reloj biológico y de metilación del DNA.
+#
+# Utilidades: Podría usarse para calcular la edad del dna mitocondrial para
+# análisis posteriores
+#
+# Link: https://bioconductor.org/packages/methylclock/
+
+
+# Paquete: rmspc
+#
+# Descripcion:Multiple Sample Peak Calling, Analisis de resultados de ChIP-seq
+#
+# Observaciones:No funciona en todas las plataformas.Tiene pocas dependencias
+#
+# Link:  http://bioconductor.org/packages/rmspc/
+
+
+# Paquete: omixer
+#
+# Descripcion: Aleatorizacion de muestras y su distribucion
+#
+# Observaciones: Ya tiene 18 meses en bioconductor, y la unica duda que hay no ha
+# sido contestada, pero funciona en todas las plataformas
+#
+# Link: http://bioconductor.org/packages/Omixer/
+
+
+# Paquete: microbiomeMarker
+
+# Resumen: Facilita la identificacion de marcadores microbianos.
+
+# Plataformas: todas
+
+# Respuesta: en ocasiones
+
+# Link: https://bioconductor.org/packages/microbiomeMarker/
+
+
+# Paquete: MicrobiomeProfiler
+
+# Resumen: Ayuda al analisis de datos bacterianos.
+
+# Plataformas: todas
+
+# Respuesta: no
+
+# Link: https://bioconductor.org/packages/MicrobiomeProfiler/
+
+
+# Paquete: spatzie
+#
+# Descripción: identificación de motivos co-enriquecidos de interacciones
+# enhancer-promotor
+#
+# Utilidades: Definir grupos de interacciones y buscar pares de motivos
+# enriquecidos.
+#
+# Funciona en todas las plataformas.
+#
+# Link: http://bioconductor.org/packages/release/bioc/html/spatzie.html
+
+
+# Paquete: monaLisa
+
+# Descripción: Trabaja con motivos en el análisis de datos genómicos
+
+# Utilidades: Anotación de secuencias o  regiones genómicas con motivos que
+# originan cambios en la expresión
+
+# Funciona en todas las plataformas
+
+# Link: http://bioconductor.org/packages/release/bioc/html/monaLisa.html
